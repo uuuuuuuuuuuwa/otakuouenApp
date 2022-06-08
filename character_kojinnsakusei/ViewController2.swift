c74438f97699140d93a182a2ea41bd9ab9a0836b
uchinowakana <zhicaineiye312@gmail.com>
2022-06-08 14:21:38 +0900

    format

--- character_kojinnsakusei/ViewController2.swift
+++ character_kojinnsakusei/ViewController2.swift
@@ -10,69 +10,69 @@ import AVFoundation
 
 
 
-    class ViewController2: UIViewController,UITableViewDelegate {
+class ViewController2: UIViewController,UITableViewDelegate {
+    
+    //StoryBoardで扱う TableViewを宣言
+    
+    @IBOutlet var table: UITableView!
+    
+    
+    var NameArray = [String]()
+    var selectedIndex = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
         
-        //StoryBoardで扱う TableViewを宣言
-        
-        @IBOutlet var table: UITableView!
-        
-        
-        var NameArray = [String]()
-        var selectedIndex = 0
-       
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            // Do any additional setup after loading the view.
-            
-            //テーブルビューのデータソースメソッドはviewcontrollerクラスに書くよ、と言う設定
-            table.dataSource = self
-            table.delegate = self
-            
-            
-           
-            NameArray = ["a","b","c","d","e"]
-        }
+        //テーブルビューのデータソースメソッドはviewcontrollerクラスに書くよ、と言う設定
+        table.dataSource = self
+        table.delegate = self
         
         
-        //セルの数を設定
         
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            //return 10
-            
-            //セルの数をsongNameArrayの数にする
-          return NameArray.count
-        }
-        
-        //ID付きのセルを取得して、セル付属textLabelに「テスト」と表示させてみる
-        
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
-            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
-            
-           
-            cell?.textLabel?.text = NameArray[indexPath.row]
-            
-            
-       //     cell?.textLabel?.text = "tesuto"
-            return cell!
-
-            
-
-        }
-        
-        func tableView(_ table: UITableView, didSelectRowAt indexPath:IndexPath)  {
-            print("\(NameArray[indexPath.row])が選ばれました！")
-            selectedIndex = indexPath.row
-            
-            performSegue(withIdentifier: "toContents", sender: nil)
+        NameArray = ["a","b","c","d","e"]
     }
-        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-            let contentVC = segue.destination as? ContentsViewController
-            contentVC?.selectedlndex = selectedIndex
-            
-        }
-      
+    
+    
+    //セルの数を設定
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return 10
+        
+        //セルの数をsongNameArrayの数にする
+        return NameArray.count
     }
+    
+    //ID付きのセルを取得して、セル付属textLabelに「テスト」と表示させてみる
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        
+        cell?.textLabel?.text = NameArray[indexPath.row]
+        
+        
+        //     cell?.textLabel?.text = "tesuto"
+        return cell!
+        
+        
+        
+    }
+    
+    func tableView(_ table: UITableView, didSelectRowAt indexPath:IndexPath)  {
+        print("\(NameArray[indexPath.row])が選ばれました！")
+        selectedIndex = indexPath.row
+        
+        performSegue(withIdentifier: "toContents", sender: nil)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let contentVC = segue.destination as? ContentsViewController
+        contentVC?.selectedlndex = selectedIndex
+        
+    }
+    
+}
 extension ViewController2: UITableViewDataSource{
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let action = UIContextualAction(style: .destructive,
@@ -91,10 +91,10 @@ extension ViewController2: UITableViewDataSource{
                                        preferredStyle: .alert)
         dialog.addAction(UIAlertAction(title: "削除", style: .default, handler: { (_) in
             self.NameArray.remove(at: indexPath.row)
-           // tableView.beginUpdates()
+            // tableView.beginUpdates()
             self.table.deleteRows(at: [indexPath], with: .automatic)
             self.table.reloadData()
-        //tableView.endUpdates()
+            //tableView.endUpdates()
         }))
         dialog.addAction(UIAlertAction(title: "戻る", style: .cancel, handler: nil))
         self.present(dialog, animated: true, completion: nil)
@@ -102,22 +102,22 @@ extension ViewController2: UITableViewDataSource{
 }
 
 
-       
 
 
 
-        // Do any additional setup after loading the view.
-    
-    
 
-    /*
-    // MARK: - Navigation
+// Do any additional setup after loading the view.
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+
+
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destination.
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 
